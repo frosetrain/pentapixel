a628478d3f2a33c77536a4fde30a177b3d2dc913
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2023-06-21 11:46:24 +0800

    Add color picker

---
+++ README.md
@@ -0,0 +1,10 @@
+(Basic) Color Picker
+(Basic) Clear Canvas Button
+(Basic) Paint whole canvas in particular color
+(Basic) Turn on/off grid lines using radio box
+(Basic) Paint in random colors
+(Intermediate) Alternate white/grey pattern to represent transparent layer
+(Intermediate) Separate landing page to allow user to control the number of rows and columns
+(Intermediate) Switch color of any 2 pixels through mousePressed and mouseReleased actions
+(Advanced) Implement layers
+(Advanced) Draw rect box through mousePressed and mouseReleased actions
--- sketch.js
+++ sketch.js
@@ -1,13 +1,5 @@
-let pixelSize = 10;
-let gridWidth = 16;
-let gridHeight = 9;
-let xOffset = 0;
-let yOffset = 0;
-let xGap = 0;
-let yGap = 0;
-let moveView = false;
-
-let arr = [];
+const gridWidth = 16;
+const gridHeight = 9;
 const transition = " transition duration-200 ease-in-out";
 const sunIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
@@ -17,14 +9,42 @@ const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0
   <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
 </svg>
 `;
-const viewIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"/>
-</svg>`;
-const brushIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-brush" viewBox="0 0 16 16">
-  <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z"/>
-</svg>`;
+const toolIcons = [
+    // move
+    `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"/></svg>`,
+    // brush
+    `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-brush" viewBox="0 0 16 16"><path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z"/></svg>`,
+    // rect
+    `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/></svg>`,
+    // line
+    `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-slash-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/></svg>`,
+];
+const twColors = [
+    "#ef4444",
+    "#f97316",
+    "#eab308",
+    "#22c55e",
+    "#3b82f6",
+    "#d946ef",
+    "#111827",
+    "#6b7280",
+    "#f3f4f6",
+];
 
+let pixelSize = 10;
+let xOffset = 0;
+let yOffset = 0;
+let xGap = 0;
+let yGap = 0;
+let tool = 0;
+let arr = [];
 let colorModeBtn;
+let toolCallbacks = [];
+let toolButtons = [];
+let colorCallbacks = [];
+let colorButtons = [];
+let color = 0;
+let showColorPicker;
 
 function switchTheme() {
     localStorage.theme = localStorage.theme === "dark" ? "light" : "dark";
@@ -41,17 +61,12 @@ function switchTheme() {
     }
 }
 
-function toggleViewMode() {
-    if (moveView) moveView = false;
-    else moveView = true;
-}
-
 function setup() {
     arr = [];
     for (let y = 0; y < gridHeight; ++y) {
         narr = [];
         for (let x = 0; x < gridWidth; ++x) {
-            c = "#808080";
+            c = "#6b7280";
             narr.push(c);
         }
         arr.push(narr);
@@ -59,7 +74,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     headerbar = createDiv();
     headerbar.position(0, 0);
-    headerbar.class("flex justify-between w-screen m-4");
+    headerbar.class("flex justify-between w-screen p-4");
     toolDiv = createDiv();
     toolDiv.parent(headerbar);
     toolDiv.class(
@@ -75,26 +90,64 @@ function setup() {
     toolFlex = createDiv();
     toolFlex.parent(toolDiv);
     toolFlex.class("flex");
-    brushButton = createButton();
-    brushButton.html(brushIcon);
-    brushButton.parent(toolFlex);
-    brushButton.class(
-        "rounded-md m-1 p-2 bg-white text-black dark:text-white dark:bg-gray-600 shadow" +
-            transition
-    );
-    viewButton = createButton();
-    viewButton.html(viewIcon);
-    viewButton.parent(toolFlex);
-    viewButton.class(
-        "rounded-md text-black dark:text-white m-1 p-2" + transition
-    );
+
     colorModeBtn = createButton();
     colorModeBtn.parent(headerbar);
     colorModeBtn.class(
-        "my-auto rounded-full bg-accent-300 hover:bg-accent-400 p-2 shadow" +
+        "text-black dark:text-white my-auto rounded-full dark:bg-accent-700 dark:hover:bg-accent-600 bg-accent-300 hover:bg-accent-400 p-2 shadow" +
             transition
     );
     colorModeBtn.mousePressed(switchTheme);
+
+    // Populate the tool bar
+    for (let i = 0; i < 4; ++i) {
+        b = createButton();
+        b.html(toolIcons[i]);
+        b.parent(toolFlex);
+        b.class("rounded-md text-black dark:text-white m-1 p-2" + transition);
+        const callback = function () {
+            toolButtons[tool].removeClass("bg-white");
+            toolButtons[tool].removeClass("dark:bg-gray-600");
+            toolButtons[tool].removeClass("shadow");
+            tool = i;
+            toolButtons[tool].addClass("bg-white dark:bg-gray-600 shadow");
+            if (tool === 0) {
+                colorPickerDiv.addClass("hidden");
+                console.log("hide");
+            } else {
+                colorPickerDiv.removeClass("hidden");
+            }
+        };
+        b.mousePressed(callback);
+        toolButtons.push(b);
+    }
+    toolButtons[0].addClass("bg-white dark:text-white dark:bg-gray-600 shadow");
+
+    // Create the color picker
+    bottomBar = createDiv();
+    bottomBar.position(0, height - 100);
+    bottomBar.class("flex w-screen p-4 gap-8");
+    colorPickerDiv = createDiv();
+    colorPickerDiv.parent(bottomBar);
+    colorPickerDiv.class(
+        "hidden flex w-min shadow rounded-lg bg-gray-200 dark:bg-gray-800 p-3 gap-4" +
+            transition
+    );
+    for (let i = 0; i < twColors.length; ++i) {
+        b = createButton("");
+        b.class("rounded-full w-6 h-6 my-auto");
+        b.parent(colorPickerDiv);
+        b.style("background-color", twColors[i]);
+        const callback = function () {
+            colorButtons[color].removeClass("shadow");
+            colorButtons[color].removeClass("scale-125");
+            color = i;
+            colorButtons[color].addClass("shadow scale-125");
+        };
+        b.mousePressed(callback);
+        colorButtons.push(b);
+    }
+    colorButtons[0].addClass("shadow scale-125");
 
     if (
         localStorage.theme === "dark" ||
@@ -116,7 +169,6 @@ function setup() {
     }
 
     frameRate(30);
-    // fill("red");
     drawGrid();
 }
 
@@ -143,13 +195,8 @@ function mouseWheel(event) {
     drawGrid();
 }
 
-function mouseDragged(event) {
-    if (moveView) {
-        // console.log(event);
-        xOffset += event.movementX;
-        yOffset += event.movementY;
-        drawGrid();
-    } else if (
+function inCanvas() {
+    return (
         mouseX > (width - pixelSize * gridWidth) / 2 + xOffset &&
         mouseX <
             (width - pixelSize * gridWidth) / 2 +
@@ -160,12 +207,26 @@ function mouseDragged(event) {
             (height - pixelSize * gridHeight) / 2 +
                 yOffset +
                 pixelSize * gridHeight
-    ) {
+    );
+}
+
+function mouseDragged(event) {
+    if (tool === 0) {
+        // Move tool
+        xOffset += event.movementX;
+        yOffset += event.movementY;
+        drawGrid();
+    } else if (tool === 1 && inCanvas()) {
+        // Brush tool
         canX = floor((mouseX - xGap) / pixelSize);
         canY = floor((mouseY - yGap) / pixelSize);
-        // console.log(canX, canY);
-        arr[canY][canX] = "#00ff00";
+        arr[canY][canX] = twColors[color];
         drawGrid();
+    }
+}
+
+function mousePressed() {
+    if (tool === 2 && inCanvas()) {
     }
 }
 
